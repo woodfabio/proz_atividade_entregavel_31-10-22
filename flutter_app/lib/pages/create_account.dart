0f692eb02a42f64c0d6228fe44a7f0e12fded7a1
wood <fabio.wood.rs@gmail.com>
2022-10-18 14:07:11 -0300

    commit_final_codigo_nao_terminado_mas_txt_completo

--- flutter_app/lib/pages/create_account.dart
+++ flutter_app/lib/pages/create_account.dart
@@ -1,12 +1,24 @@
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_app/widgets/custom_textformfield.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
+// ===================================================================================================================================
+// ATENCAO: por razoes de otimizacao de tempo este codigo nao foi terminado, mas o .txt desta aula esta bem completo <----------------
+// ===================================================================================================================================
 
 class CreateAccount extends StatefulWidget {
-  const CreateAccount({
+
+  final _phoneMaskFormatter = MaskTextInputFormatter(
+    mask: '+00 (00) 00000-0000',
+    filter: {"0": RegExp(r'[0-9]')},
+    type: MaskAutoCompletionType.lazy,
+    );
+
+  CreateAccount({
     super.key,
-  })
+  });
 
   @override
   State<CreateAccount> createState() => _CreateAccountState();
@@ -17,28 +29,33 @@ class _CreateAccountState extends State<CreateAccount> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Registre-se no ContactApp',
-              style: TextStyle(fontSize: 15),
-            ),
-            const SizedBox(height: 8.0,),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: const TextField(
-                decoration: InputDecoration(
-                  label: Text('Nome'),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                    )
-                  ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              const Text(
+                'Registre-se no ContactApp',
+                style: TextStyle(fontSize: 15),
               ),
-            )
-          ],
+              const SizedBox(height: 8.0,),
+              const CustTextFormField(
+                label: 'Nome',
+                textInputType: TextInputType.name,
+                textInputAction: TextInputAction.next,
+              ),
+              const SizedBox(height: 16.0,), // espacamento entre campos
+              CustTextFormField(
+                label: 'Telefone',
+                textInputType: TextInputType.phone,
+                inputFormatters: [ widget._phoneMaskFormatter ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
